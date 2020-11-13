554c4a57dfd3e29ab381740cc72162515ac30413
Andrea Sbarra <andrea.ale.sbarra@gmail.com>
2020-11-13 17:58:47 +0100

    COMMERCE-4977 headless-commerce-admin-shipment-test added logic for CRUD tests

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-shipment-test/src/testIntegration/java/com/liferay/headless/commerce/admin/shipment/resource/v1_0/test/ShipmentItemResourceTest.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-shipment-test/src/testIntegration/java/com/liferay/headless/commerce/admin/shipment/resource/v1_0/test/ShipmentItemResourceTest.java
@@ -15,14 +15,211 @@
 package com.liferay.headless.commerce.admin.shipment.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.commerce.currency.model.CommerceCurrency;
+import com.liferay.commerce.currency.test.util.CommerceCurrencyTestUtil;
+import com.liferay.commerce.inventory.model.CommerceInventoryWarehouse;
+import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.commerce.model.CommerceOrderItem;
+import com.liferay.commerce.model.CommerceShipment;
+import com.liferay.commerce.model.CommerceShipmentItem;
+import com.liferay.commerce.product.model.CPInstance;
+import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.commerce.product.service.CPInstanceLocalServiceUtil;
+import com.liferay.commerce.product.test.util.CPTestUtil;
+import com.liferay.commerce.service.CommerceShipmentItemLocalServiceUtil;
+import com.liferay.commerce.service.CommerceShipmentLocalServiceUtil;
+import com.liferay.commerce.test.util.CommerceInventoryTestUtil;
+import com.liferay.commerce.test.util.CommerceTestUtil;
+import com.liferay.headless.commerce.admin.shipment.client.dto.v1_0.ShipmentItem;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
 
-import org.junit.Ignore;
+import java.math.BigDecimal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.runner.RunWith;
 
 /**
  * @author Andrea Sbarra
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class ShipmentItemResourceTest extends BaseShipmentItemResourceTestCase {
+
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_user = UserTestUtil.addUser(testCompany);
+
+		PermissionThreadLocal.setPermissionChecker(
+			PermissionCheckerFactoryUtil.create(_user));
+
+		PrincipalThreadLocal.setName(_user.getUserId());
+
+		_commerceCurrency = CommerceCurrencyTestUtil.addCommerceCurrency(
+			testCompany.getCompanyId());
+
+		_commerceChannel = CommerceTestUtil.addCommerceChannel(
+			testGroup.getGroupId(), _commerceCurrency.getCode());
+
+		BigDecimal value = BigDecimal.valueOf(RandomTestUtil.nextDouble());
+
+		_commerceOrder = CommerceTestUtil.createCommerceOrderForShipping(
+			_user.getUserId(), _commerceChannel.getGroupId(),
+			_commerceCurrency.getCommerceCurrencyId(), value);
+
+		BigDecimal price = BigDecimal.valueOf(RandomTestUtil.randomDouble());
+
+		CPInstance cpInstance = CPTestUtil.addCPInstanceWithRandomSku(
+			_commerceOrder.getGroupId(), price);
+
+		CPInstanceLocalServiceUtil.updateCPInstance(cpInstance);
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			testCompany.getCompanyId(), testGroup.getGroupId(),
+			_user.getUserId());
+
+		_commerceInventoryWarehouse =
+			CommerceInventoryTestUtil.addCommerceInventoryWarehouse(
+				_serviceContext);
+
+		CommerceTestUtil.addWarehouseCommerceChannelRel(
+			_commerceInventoryWarehouse.getCommerceInventoryWarehouseId(),
+			_commerceChannel.getCommerceChannelId());
+
+		CommerceInventoryTestUtil.addCommerceInventoryWarehouseItem(
+			_user.getUserId(), _commerceInventoryWarehouse, cpInstance.getSku(),
+			10);
+
+		CommerceTestUtil.addCommerceOrderItem(
+			_commerceOrder.getCommerceOrderId(), cpInstance.getCPInstanceId(),
+			4);
+
+		_commerceShipment =
+			CommerceShipmentLocalServiceUtil.addCommerceShipment(
+				_commerceOrder.getCommerceOrderId(), _serviceContext);
+	}
+
+	@Override
+	protected String[] getAdditionalAssertFieldNames() {
+		return super.getAdditionalAssertFieldNames();
+	}
+
+	@Override
+	protected ShipmentItem testDeleteShipmentItem_addShipmentItem()
+		throws Exception {
+
+		return _addShipmentItem();
+	}
+
+	@Override
+	protected ShipmentItem testGetShipmentItem_addShipmentItem()
+		throws Exception {
+
+		return _addShipmentItem();
+	}
+
+	@Override
+	protected ShipmentItem testGetShipmentItemsPage_addShipmentItem(
+			Long shipmentId, ShipmentItem shipmentItem)
+		throws Exception {
+
+		return _addShipmentItem();
+	}
+
+	@Override
+	protected ShipmentItem testGraphQLShipmentItem_addShipmentItem()
+		throws Exception {
+
+		return _addShipmentItem();
+	}
+
+	@Override
+	protected ShipmentItem testPatchShipmentItem_addShipmentItem()
+		throws Exception {
+
+		return _addShipmentItem();
+	}
+
+	@Override
+	protected ShipmentItem testPostShipmentItem_addShipmentItem(
+			ShipmentItem shipmentItem)
+		throws Exception {
+
+		return _addShipmentItem();
+	}
+
+	private ShipmentItem _addShipmentItem() throws Exception {
+		List<CommerceOrderItem> commerceOrderItems =
+			_commerceOrder.getCommerceOrderItems();
+
+		CommerceOrderItem commerceOrderItem = commerceOrderItems.get(0);
+
+		_commerceShipmentItem =
+			CommerceShipmentItemLocalServiceUtil.addCommerceShipmentItem(
+				_commerceShipment.getCommerceShipmentId(),
+				commerceOrderItem.getCommerceOrderItemId(),
+				_commerceInventoryWarehouse.getCommerceInventoryWarehouseId(),
+				4, _serviceContext);
+
+		_commerceShipmentItems.add(_commerceShipmentItem);
+
+		return _toShipmentItem(_commerceShipmentItem);
+	}
+
+	private ShipmentItem _toShipmentItem(
+		CommerceShipmentItem commerceShipmentItem) {
+
+		return new ShipmentItem() {
+			{
+				createDate = commerceShipmentItem.getCreateDate();
+				id = commerceShipmentItem.getCommerceShipmentItemId();
+				modifiedDate = commerceShipmentItem.getModifiedDate();
+				orderItemId = commerceShipmentItem.getCommerceOrderItemId();
+				quantity = commerceShipmentItem.getQuantity();
+				shipmentId = commerceShipmentItem.getCommerceShipmentId();
+				userName = commerceShipmentItem.getUserName();
+				warehouseId =
+					commerceShipmentItem.getCommerceInventoryWarehouseId();
+			}
+		};
+	}
+
+	@DeleteAfterTestRun
+	private CommerceChannel _commerceChannel;
+
+	@DeleteAfterTestRun
+	private CommerceCurrency _commerceCurrency;
+
+	@DeleteAfterTestRun
+	private CommerceInventoryWarehouse _commerceInventoryWarehouse;
+
+	@DeleteAfterTestRun
+	private CommerceOrder _commerceOrder;
+
+	@DeleteAfterTestRun
+	private CommerceShipment _commerceShipment;
+
+	@DeleteAfterTestRun
+	private CommerceShipmentItem _commerceShipmentItem;
+
+	@DeleteAfterTestRun
+	private final List<CommerceShipmentItem> _commerceShipmentItems =
+		new ArrayList<>();
+
+	private ServiceContext _serviceContext;
+
+	@DeleteAfterTestRun
+	private User _user;
+
 }
--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-shipment-test/src/testIntegration/java/com/liferay/headless/commerce/admin/shipment/resource/v1_0/test/ShipmentResourceTest.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-shipment-test/src/testIntegration/java/com/liferay/headless/commerce/admin/shipment/resource/v1_0/test/ShipmentResourceTest.java
@@ -15,14 +15,207 @@
 package com.liferay.headless.commerce.admin.shipment.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.commerce.currency.model.CommerceCurrency;
+import com.liferay.commerce.currency.test.util.CommerceCurrencyTestUtil;
+import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.commerce.model.CommerceShipment;
+import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.commerce.service.CommerceShipmentLocalServiceUtil;
+import com.liferay.commerce.test.util.CommerceTestUtil;
+import com.liferay.headless.commerce.admin.shipment.client.dto.v1_0.Shipment;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.odata.entity.EntityField;
 
-import org.junit.Ignore;
+import java.math.BigDecimal;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.runner.RunWith;
 
 /**
  * @author Andrea Sbarra
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class ShipmentResourceTest extends BaseShipmentResourceTestCase {
+
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_user = UserTestUtil.addUser(testCompany);
+
+		PermissionThreadLocal.setPermissionChecker(
+			PermissionCheckerFactoryUtil.create(_user));
+
+		PrincipalThreadLocal.setName(_user.getUserId());
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			testCompany.getCompanyId(), testGroup.getGroupId(),
+			_user.getUserId());
+
+		_commerceCurrency = CommerceCurrencyTestUtil.addCommerceCurrency(
+			testCompany.getCompanyId());
+
+		_commerceChannel = CommerceTestUtil.addCommerceChannel(
+			testGroup.getGroupId(), _commerceCurrency.getCode());
+
+		BigDecimal value = BigDecimal.valueOf(RandomTestUtil.nextDouble());
+
+		_commerceOrder = CommerceTestUtil.createCommerceOrderForShipping(
+			_user.getUserId(), _commerceChannel.getGroupId(),
+			_commerceCurrency.getCommerceCurrencyId(), value);
+	}
+
+	@Override
+	protected String[] getAdditionalAssertFieldNames() {
+		return new String[] {
+			"accountId", "addressId", "shippingMethodId", "shippingOptionName"
+		};
+	}
+
+	@Override
+	protected Collection<EntityField> getEntityFields() throws Exception {
+		return new ArrayList<>();
+	}
+
+	protected Shipment randomShipment() throws Exception {
+		return new Shipment() {
+			{
+				accountId = _commerceOrder.getCommerceAccountId();
+				addressId = _commerceOrder.getShippingAddressId();
+				carrier = StringUtil.toLowerCase(RandomTestUtil.randomString());
+				createDate = RandomTestUtil.nextDate();
+				expectedDate = RandomTestUtil.nextDate();
+				modifiedDate = RandomTestUtil.nextDate();
+				orderId = _commerceOrder.getCommerceOrderId();
+				shippingDate = RandomTestUtil.nextDate();
+				shippingMethodId = _commerceOrder.getCommerceShippingMethodId();
+				shippingOptionName = _commerceOrder.getShippingOptionName();
+				trackingNumber = StringUtil.toLowerCase(
+					RandomTestUtil.randomString());
+				userName = _commerceOrder.getUserName();
+			}
+		};
+	}
+
+	@Override
+	protected Shipment testDeleteShipment_addShipment() throws Exception {
+		return _addShipment();
+	}
+
+	@Override
+	protected Shipment testGetShipment_addShipment() throws Exception {
+		return _addShipment();
+	}
+
+	@Override
+	protected Shipment testGetShipmentsPage_addShipment(Shipment shipment)
+		throws Exception {
+
+		return _addShipment();
+	}
+
+	@Override
+	protected Shipment testGraphQLShipment_addShipment() throws Exception {
+		return _addShipment();
+	}
+
+	@Override
+	protected Shipment testPatchShipment_addShipment() throws Exception {
+		return _addShipment();
+	}
+
+	@Override
+	protected Shipment testPostShipment_addShipment(Shipment shipment)
+		throws Exception {
+
+		return _addShipment();
+	}
+
+	@Override
+	protected Shipment testPostShipmentDeliver_addShipment(Shipment shipment)
+		throws Exception {
+
+		return _addShipment();
+	}
+
+	@Override
+	protected Shipment testPostShipmentFinishProcessing_addShipment(
+			Shipment shipment)
+		throws Exception {
+
+		return _addShipment();
+	}
+
+	@Override
+	protected Shipment testPostShipmentShip_addShipment(Shipment shipment)
+		throws Exception {
+
+		return _addShipment();
+	}
+
+	private Shipment _addShipment() throws Exception {
+		_commerceShipment =
+			CommerceShipmentLocalServiceUtil.addCommerceShipment(
+				_commerceOrder.getCommerceOrderId(), _serviceContext);
+
+		_commerceShipments.add(_commerceShipment);
+
+		return _toShipment(_commerceShipment);
+	}
+
+	private Shipment _toShipment(CommerceShipment commerceShipment)
+		throws Exception {
+
+		return new Shipment() {
+			{
+				accountId = commerceShipment.getCommerceAccountId();
+				addressId = commerceShipment.getCommerceAddressId();
+				carrier = commerceShipment.getCarrier();
+				createDate = commerceShipment.getCreateDate();
+				expectedDate = commerceShipment.getExpectedDate();
+				id = commerceShipment.getCommerceShipmentId();
+				modifiedDate = commerceShipment.getModifiedDate();
+				shippingDate = commerceShipment.getShippingDate();
+				shippingMethodId =
+					commerceShipment.getCommerceShippingMethodId();
+				shippingOptionName = commerceShipment.getShippingOptionName();
+				trackingNumber = commerceShipment.getTrackingNumber();
+				userName = commerceShipment.getUserName();
+			}
+		};
+	}
+
+	@DeleteAfterTestRun
+	private CommerceChannel _commerceChannel;
+
+	@DeleteAfterTestRun
+	private CommerceCurrency _commerceCurrency;
+
+	@DeleteAfterTestRun
+	private CommerceOrder _commerceOrder;
+
+	@DeleteAfterTestRun
+	private CommerceShipment _commerceShipment;
+
+	@DeleteAfterTestRun
+	private final List<CommerceShipment> _commerceShipments = new ArrayList<>();
+
+	private ServiceContext _serviceContext;
+
+	@DeleteAfterTestRun
+	private User _user;
+
 }
