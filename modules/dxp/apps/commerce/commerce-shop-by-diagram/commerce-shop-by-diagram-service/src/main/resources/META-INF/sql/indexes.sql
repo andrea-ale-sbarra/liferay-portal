9b4e04d23d70fc988bfe15d4df33a4487655830b
Andrea Sbarra <andrea.ale.sbarra@gmail.com>
2021-06-30 17:56:09 +0200

    COMMERCE-5661 commerce-shop-by-diagram-service removed wrong implementations

--- modules/dxp/apps/commerce/commerce-shop-by-diagram/commerce-shop-by-diagram-service/src/main/resources/META-INF/sql/indexes.sql
+++
@@ -1,4 +0,0 @@
-create index IX_F91BABC on CPDefinitionDiagramEntry (CPDefinitionDiagramSettingsId);
-
-create unique index IX_C8A8782B on CPDefinitionDiagramSettings (CPDefinitionId);
-create index IX_7686859D on CPDefinitionDiagramSettings (assetCategoryId);
