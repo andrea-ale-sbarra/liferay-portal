553750fe9b101f2a388fc7ec2c76bdc28c7cdab1
Gianmarco Brunialti <gianmarco.brunialti@liferay.com>
2024-06-10 15:27:09 +0200

    LPD-22571 Autogenerated Code (gw buildREST)

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/graphql/query/v1_0/Query.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/graphql/query/v1_0/Query.java
@@ -2026,7 +2026,7 @@ public class Query {
 	/**
 	 * Invoke this method with the command line:
 	 *
-	 * curl -H 'Content-Type: text/plain; charset=utf-8' -X 'POST' 'http://localhost:8080/o/graphql' -d $'{"query": "query {specification(id: ___){description, facetable, id, key, optionCategory, priority, title}}"}' -u 'test@liferay.com:test'
+	 * curl -H 'Content-Type: text/plain; charset=utf-8' -X 'POST' 'http://localhost:8080/o/graphql' -d $'{"query": "query {specification(id: ___){description, facetable, id, key, optionCategory, pickListId, priority, title}}"}' -u 'test@liferay.com:test'
 	 */
 	@GraphQLField
 	public Specification specification(@GraphQLName("id") Long id)
