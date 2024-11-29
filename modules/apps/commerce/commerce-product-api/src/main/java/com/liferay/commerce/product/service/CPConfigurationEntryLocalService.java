520596cd8173ba875e8a291facf838432a830211
andrea.ale.sbarra <andrea.ale.sbarra@gmail.com>
2024-11-29 16:22:37 +0100

    LPD-42555 autogenerated code (gw buildService)

--- modules/apps/commerce/commerce-product-api/src/main/java/com/liferay/commerce/product/service/CPConfigurationEntryLocalService.java
+++ modules/apps/commerce/commerce-product-api/src/main/java/com/liferay/commerce/product/service/CPConfigurationEntryLocalService.java
@@ -81,12 +81,16 @@ public interface CPConfigurationEntryLocalService
 	public CPConfigurationEntry addCPConfigurationEntry(
 			String externalReferenceCode, long userId, long groupId,
 			long classNameId, long classPK, long cpConfigurationListId,
-			String allowedOrderQuantities, boolean backOrders,
-			long commerceAvailabilityEstimateId,
-			String cpDefinitionInventoryEngine, boolean displayAvailability,
-			boolean displayStockQuantity, String lowStockActivity,
+			long cpTaxCategoryId, String allowedOrderQuantities,
+			boolean backOrders, long commerceAvailabilityEstimateId,
+			String cpDefinitionInventoryEngine, double depth,
+			boolean displayAvailability, boolean displayStockQuantity,
+			boolean freeShipping, double height, String lowStockActivity,
 			BigDecimal maxOrderQuantity, BigDecimal minOrderQuantity,
-			BigDecimal minStockQuantity, BigDecimal multipleOrderQuantity)
+			BigDecimal minStockQuantity, BigDecimal multipleOrderQuantity,
+			boolean purchasable, boolean shippable, double shippingExtraPrice,
+			boolean shipSeparately, boolean taxExempt, boolean visible,
+			double weight, double width)
 		throws PortalException;
 
 	/**
@@ -372,12 +376,16 @@ public interface CPConfigurationEntryLocalService
 	@Indexable(type = IndexableType.REINDEX)
 	public CPConfigurationEntry updateCPConfigurationEntry(
 			String externalReferenceCode, long cpConfigurationEntryId,
-			String allowedOrderQuantities, boolean backOrders,
-			long commerceAvailabilityEstimateId,
-			String cpDefinitionInventoryEngine, boolean displayAvailability,
-			boolean displayStockQuantity, String lowStockActivity,
+			long cpTaxCategoryId, String allowedOrderQuantities,
+			boolean backOrders, long commerceAvailabilityEstimateId,
+			String cpDefinitionInventoryEngine, double depth,
+			boolean displayAvailability, boolean displayStockQuantity,
+			boolean freeShipping, double height, String lowStockActivity,
 			BigDecimal maxOrderQuantity, BigDecimal minOrderQuantity,
-			BigDecimal minStockQuantity, BigDecimal multipleOrderQuantity)
+			BigDecimal minStockQuantity, BigDecimal multipleOrderQuantity,
+			boolean purchasable, boolean shippable, double shippingExtraPrice,
+			boolean shipSeparately, boolean taxExempt, boolean visible,
+			double weight, double width)
 		throws PortalException;
 
 	@Override
