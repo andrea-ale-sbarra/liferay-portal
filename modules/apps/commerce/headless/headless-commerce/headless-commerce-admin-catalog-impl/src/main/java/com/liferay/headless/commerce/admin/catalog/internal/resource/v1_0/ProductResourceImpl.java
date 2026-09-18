71197db0c9b84a68b1f79af020b0f62fab4daa6e
Danny Situ <danny.situ@liferay.com>
2026-09-18 11:24:59 -0700

    LPD-106110 Separate the copy from the draft lookup in CPDefinition

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/resource/v1_0/ProductResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/resource/v1_0/ProductResourceImpl.java
@@ -1071,7 +1071,7 @@ public class ProductResourceImpl
 			(serviceContext.getWorkflowAction() ==
 				WorkflowConstants.ACTION_SAVE_DRAFT)) {
 
-			return _cpDefinitionService.copyCPDefinition(
+			return _cpDefinitionService.getOrCopyCPDefinition(
 				cpDefinition.getCPDefinitionId(), cpDefinition.getGroupId(),
 				WorkflowConstants.STATUS_DRAFT);
 		}
