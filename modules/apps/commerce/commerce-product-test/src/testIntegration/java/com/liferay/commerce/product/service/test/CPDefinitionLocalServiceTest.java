cf1e09939172fb987dd444897cb005d215d404ce
andrea-ale-sbarra <andrea.ale.sbarra@gmail.com>
2026-09-21 21:38:24 +0200

    LPD-106110 SF

--- modules/apps/commerce/commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/service/test/CPDefinitionLocalServiceTest.java
+++ modules/apps/commerce/commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/service/test/CPDefinitionLocalServiceTest.java
@@ -798,8 +798,7 @@ public class CPDefinitionLocalServiceTest {
 				_cpDefinitionLocalService.fetchCPDefinition(
 					cpDefinition.getCPDefinitionId()));
 
-			Assert.assertNull(
-				_cProductLocalService.fetchCProduct(cProductId));
+			Assert.assertNull(_cProductLocalService.fetchCProduct(cProductId));
 		}
 	}
 
@@ -2524,8 +2523,7 @@ public class CPDefinitionLocalServiceTest {
 		Date expirationDate = cpDefinition.getExpirationDate();
 
 		return _cpDefinitionLocalService.updateCPDefinition(
-			cpDefinition.getCPDefinitionId(),
-			cpDefinition.getCPTaxCategoryId(),
+			cpDefinition.getCPDefinitionId(), cpDefinition.getCPTaxCategoryId(),
 			cpDefinition.isAccountGroupFilterEnabled(),
 			cpDefinition.isChannelFilterEnabled(),
 			cpDefinition.getDDMStructureKey(), cpDefinition.getDepth(),
